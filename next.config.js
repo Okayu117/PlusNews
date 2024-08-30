c08aabe26e9a00625cea992a52367b086de05e17
{yuri} <{u.ri8yy8ri.u@gmail.com}>
2024-08-30 23:24:11 +0900

    エイリアス問題

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 module.exports = {
   env: {
     GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     NEXT_PUBLIC_NEWS_API_KEY: process.env.NEXT_PUBLIC_NEWS_API_KEY,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 };
